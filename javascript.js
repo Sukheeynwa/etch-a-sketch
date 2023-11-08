bc26a67f1d50c70593ada1bc85cc87671110cdea
Sukhee <sukheearch@gmail.com>
2023-11-08 16:46:06 +0800

    made grids customizable

--- javascript.js
+++ javascript.js
@@ -1,50 +1,78 @@
 let horizontal = document.querySelector("#horizontal");
-let vertical = document.querySelector("#vertical")
-
-let grid = 32;
-
-for (let i = 1; i <= grid; i++) {
-    let i = document.createElement("div");
-    i.setAttribute("id", "horizontal");
-    vertical.append(i);
-
-    for (let j = 1; j <= grid; j++) {
-        let j = document.createElement("div");
-        j.setAttribute("id", "div");
-        i.appendChild(j);
-    };
-};
-
-let div = document.querySelectorAll("#div")
-let drag = false; 
 
 let myOpacity = 0;
-let red = 0;
-let green = 0;
-let blue = 0;
+let red = 120;
+let green = 120;
+let blue = 255;
+myOpacity = 0.1;
 
-myOpacity = 0;
+let gridPicker = document.querySelector("#grid-picker")
 
-function changeColor() {
+gridPicker.addEventListener("input", () => {
+
+    grid = Number(gridPicker.value);
+    console.log(grid)
+
+    const container = document.querySelector("#container")
+
+    while(container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+
+    let vertical = document.createElement("div");
+    vertical.setAttribute("id", "vertical")
+    container.appendChild(vertical);
+
+    for (let i = 1; i <= grid; i++) {
+
+        let i = document.createElement("div");
+        i.setAttribute("id", "horizontal");
+        vertical.append(i);
+    
+        for (let j = 1; j <= grid; j++) {
+            let j = document.createElement("div");
+            j.setAttribute("id", "div");
+            i.appendChild(j);
+        };  
+    };
+    
+    
+
+    draw()
+})
+
+function draw() {
+    let div = document.querySelectorAll("#div")
+    let drag = false; 
     div.forEach((div) => {
         div.addEventListener('mousedown', (e) => {
             div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
             div.style.opacity = Number(div.style.opacity || myOpacity) + 0.1;
             drag = true
+            console.log(e.type)
         }); 
-    
+
         div.addEventListener('mouseup', (e) => {
             drag = false
         });
-    
+
         div.addEventListener("mousemove", (e) => {
             if (drag === true) {
                 div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
                 div.style.opacity = Number(div.style.opacity || myOpacity) + 0.1;
+                console.log(e.type)
             }
         });
     }
     );
-};
+}
 
-changeColor()
+
+
+
+
+
+
+
+
+
